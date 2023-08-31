a72de6d36d4d75c8486391ebbc0600fb3f845bd3
idowudamolaolatunji <olatunjiidowuoyin06@gmail.com>
2023-08-31 18:20:46 +0100

    updated create blog

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const blogController = require('../controllers/blogController');
+const authController = require('../controllers/authController');
+
 
 const router = express.Router();
 
 router.route('/')
     .get(blogController.getAllBlog)
-    .post(blogController.createBlog)
+    .post(authController.protected ,blogController.createBlog)
 ;
 
 router.route('/:id')
